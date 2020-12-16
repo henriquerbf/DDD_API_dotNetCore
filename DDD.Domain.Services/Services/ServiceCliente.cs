d4d8adb5bf2873152daf3a36ae0c01c071e5f9c0
henriquerbf <henriquerbf@hotmail.com>
2020-12-16 00:41:22 -0300

    Services

---
+++ DDD.Domain.Services/Services/ServiceCliente.cs
@@ -0,0 +1,21 @@
+﻿using DDD.Domain.Core.Interfaces.Repositorys;
+using DDD.Domain.Core.Interfaces.Services;
+using DDD.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDD.Domain.Services.Services
+{
+    public class ServiceCliente : ServiceBase<Cliente>, IServiceCliente
+    {
+        public readonly IRepositoryCliente _repositoryCliente;
+
+        public ServiceCliente(IRepositoryCliente RepositoryCliente)
+            : base(RepositoryCliente)
+        {
+            _repositoryCliente = RepositoryCliente;
+        }
+
+    }
+}
---
+++ DDD.Domain.Services/Services/ServiceProduto.cs
@@ -0,0 +1,20 @@
+﻿using DDD.Domain.Core.Interfaces.Repositorys;
+using DDD.Domain.Core.Interfaces.Services;
+using DDD.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDD.Domain.Services.Services
+{
+    public class ServiceProduto : ServiceBase<Produto>, IServiceProduto
+    {
+        private readonly IRepositoryProduto _repositoryProduto;
+
+        public ServiceProduto(IRepositoryProduto RepositoryProduto)
+            : base(RepositoryProduto)
+        {
+            _repositoryProduto = RepositoryProduto;
+        }
+    }
+}

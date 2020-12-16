68f9c754f75a938354de3272b755516ec9d4136e
henriquerbf <henriquerbf@hotmail.com>
2020-12-16 00:42:31 -0300

    Repositories

---
+++ DDD.Infrastructure.Repository/Repositorys/RepositoryCliente.cs
@@ -0,0 +1,16 @@
+﻿using DDD.Domain.Core.Interfaces.Repositorys;
+using DDD.Domain.Models;
+using DDD.Infrastructure.Data;
+
+namespace DDD.Infrastructure.Repository.Repositorys
+{
+    public class RepositoryCliente : RepositoryBase<Cliente>, IRepositoryCliente
+    {
+        private readonly SqlContext _context;
+        public RepositoryCliente(SqlContext Context)
+            : base(Context)
+        {
+            _context = Context;
+        }
+    }
+}
---
+++ DDD.Infrastructure.Repository/Repositorys/RepositoryProduto.cs
@@ -0,0 +1,18 @@
+﻿using DDD.Domain.Core.Interfaces.Repositorys;
+using DDD.Domain.Models;
+using DDD.Infrastructure.Data;
+
+namespace DDD.Infrastructure.Repository.Repositorys
+{
+    public class RepositoryProduto : RepositoryBase<Produto>, IRepositoryProduto
+    {
+
+        private readonly SqlContext _context;
+        public RepositoryProduto(SqlContext Context)
+            : base(Context)
+        {
+            _context = Context;
+        }
+
+    }
+}
